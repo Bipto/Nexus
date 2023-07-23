c6659569f54f1a08a7795d9df2cfe25986d09a87
Adam Beardow <abeardow13@gmail.com>
2023-07-23 15:59:13 +0100

    Added comments to new buffer classes

--- NexusEngine/Core/Graphics/Buffer.hpp
+++ NexusEngine/Core/Graphics/Buffer.hpp
@@ -47,9 +47,14 @@ namespace Nexus::Graphics
         BufferUsage Usage = BufferUsage::Invalid;
     };
 
+    /// @brief A class representing an API specific vertex buffer
     class VertexBuffer
     {
     public:
+        /// @brief A constructor to create a new vertex buffer
+        /// @param description A struct representing the properties to use when creating the buffer
+        /// @param data A pointer to initial data to upload to the buffer
+        /// @param layout A const reference to a vertex buffer layout, representing the contents of the vertex buffer
         VertexBuffer(const BufferDescription &description, const void *data, const VertexBufferLayout &layout)
             : m_Description(description), m_Layout(layout) {}
 
@@ -62,16 +67,26 @@ namespace Nexus::Graphics
         /// @brief A method that returns the buffer description that was used to create the buffer
         /// @return A const reference to the BufferDescription
         const BufferDescription &GetDescription() { return m_Description; }
+
+        /// @brief A method that returns the layout of the contents of the buffer
+        /// @return A const reference to the VertexBufferLayout
         const VertexBufferLayout &GetLayout() { return m_Layout; }
 
     protected:
+        /// @brief A struct containing the properties that were used when creating the buffer
         BufferDescription m_Description;
+
+        /// @brief A struct containing the layout of the vertex buffer
         VertexBufferLayout m_Layout;
     };
 
+    /// @brief A class representing an API specific index buffer
     class IndexBuffer
     {
     public:
+        /// @brief A constructor to create a new index buffer
+        /// @param description A struct representing the properties to use when creating the buffer
+        /// @param data A pointer to initial data to upload to the buffer
         IndexBuffer(const BufferDescription &description, const void *data)
             : m_Description(description) {}
 
@@ -86,12 +101,17 @@ namespace Nexus::Graphics
         const BufferDescription &GetDescription() { return m_Description; }
 
     protected:
+        /// @brief A struct containing the properties that were used when creating the buffer
         BufferDescription m_Description;
     };
 
+    /// @brief A class representing an API specific uniform buffer
     class UniformBuffer
     {
     public:
+        /// @brief A constructor to create a new uniform buffer
+        /// @param description A struct representing the properties to use when creating the buffer
+        /// @param data A pointer to initial data to upload to the buffer
         UniformBuffer(const BufferDescription &description, const void *data)
             : m_Description(description) {}
 
@@ -106,6 +126,7 @@ namespace Nexus::Graphics
         const BufferDescription &GetDescription() { return m_Description; }
 
     protected:
+        /// @brief A struct containing the properties that were used when creating the buffer
         BufferDescription m_Description;
     };
 }
