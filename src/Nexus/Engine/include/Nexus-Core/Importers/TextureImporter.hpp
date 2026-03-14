ffa6ecf7bafc318e4d3d0bc1cb2af8d58fdf5356
Adam Beardow <abeardow13@gmail.com>
2026-03-14 17:56:08 +0000

    Added TextureImporter

---
+++ src/Nexus/Engine/include/Nexus-Core/Importers/TextureImporter.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <expected>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "Nexus-Core/nxpch.hpp"
+#include "RHI/PixelFormat.hpp"
+
+namespace Nexus
+{
+	enum class PixelType
+	{
+		Pixel8Bit,
+		Pixel16Bit,
+		PixelFloat,
+	};
+
+	struct TextureInfo
+	{
+		uint32_t  Width	   = 0;
+		uint32_t  Height   = 0;
+		uint32_t  Channels = 0;
+		PixelType Type	   = PixelType::Pixel8Bit;
+	};
+
+	struct TextureData
+	{
+		TextureInfo			   Info	  = {};
+		std::vector<std::byte> Pixels = {};
+	};
+
+	class NX_API TextureImporter
+	{
+	  public:
+		TextureImporter();
+
+		[[nodiscard]] std::expected<TextureInfo, std::string> GetTextureInfoFromDisk(std::string_view path) const;
+
+		[[nodiscard]] std::expected<TextureInfo, std::string> GetTextureInfoFromMemory(std::span<std::byte> buffer) const;
+
+		[[nodiscard]] std::expected<TextureData, std::string> LoadImageFromDisk(std::string_view		path,
+																				bool					flipVertically,
+																				std::optional<uint32_t> desiredChannels) const;
+
+		[[nodiscard]] std::expected<TextureData, std::string> LoadImageFromMemory(std::span<std::byte>	  buffer,
+																				  bool					  flipVertically,
+																				  std::optional<uint32_t> desiredChannels) const;
+	};
+}	 // namespace Nexus
