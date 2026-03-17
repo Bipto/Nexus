2d3f7962311ee88f175f3ec5d8271fa999565b14
Adam Beardow <abeardow13@gmail.com>
2026-03-17 17:54:07 +0000

    Moved shader reflection data into its own file

--- src/Nexus/RHI/include/RHI/ShaderModule.hpp
+++ src/Nexus/RHI/include/RHI/ShaderModule.hpp
@@ -3,67 +3,11 @@
 #include "RHI/RHI-Core.hpp"
 #include "RHI/ResourceSet.hpp"
 #include "RHI/ShaderDataType.hpp"
+#include "RHI/ShaderReflectionData.hpp"
 #include "RHI/ShaderResources.hpp"
 
 namespace Nexus::Graphics
 {
-	struct ReflectedResource
-	{
-		ReflectedShaderDataType Type		   = ReflectedShaderDataType::UniformBuffer;
-		std::string				BlockName	   = {};
-		std::string				InstanceName   = {};
-		ResourceDimension		Dimension	   = ResourceDimension::NoDimension;
-		StorageResourceAccess	ResourceAccess = StorageResourceAccess::NoAccess;
-		uint32_t				DescriptorSet  = 0;
-		uint32_t				BindingPoint   = 0;
-		uint32_t				BindingCount   = 0;
-		uint32_t				RegisterSpace  = 0;
-	};
-
-	struct Attribute
-	{
-		std::string				Name		= {};
-		ReflectedShaderDataType Type		= {};
-		uint32_t				Binding		= {};
-		uint32_t				StreamIndex = {};
-	};
-
-	struct ReflectedBufferMember
-	{
-		std::string				Name	  = {};
-		size_t					Offset	  = 0;
-		size_t					Size	  = 0;
-		std::optional<uint32_t> ArraySize = {};
-		ShaderDataType			Type	  = {};
-	};
-
-	struct ReflectedUniformBuffer
-	{
-		std::string						   Name	   = {};
-		std::vector<ReflectedBufferMember> Members = {};
-	};
-
-	struct ReflectedStorageBuffer
-	{
-		std::string						   Name	   = {};
-		std::vector<ReflectedBufferMember> Members = {};
-	};
-
-	struct ShaderReflectionData
-	{
-		std::vector<Attribute>				Inputs;
-		std::vector<Attribute>				Outputs;
-		std::vector<ReflectedUniformBuffer> UniformBuffers = {};
-		std::vector<ReflectedStorageBuffer> StorageBuffers = {};
-		std::vector<ReflectedResource>		Resources	   = {};
-	};
-
-	struct ShaderAttribute
-	{
-		std::string	   Name;
-		ShaderDataType DataType;
-	};
-
 	struct ShaderModuleSpecification
 	{
 		std::string			  DebugName = "ShaderModule";
---
+++ src/Nexus/RHI/include/RHI/ShaderReflectionData.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "RHI/ShaderDataType.hpp"
+#include "RHI/ShaderResources.hpp"
+
+namespace Nexus::Graphics
+{
+	struct ReflectedResource
+	{
+		ReflectedShaderDataType Type		   = ReflectedShaderDataType::UniformBuffer;
+		std::string				BlockName	   = {};
+		std::string				InstanceName   = {};
+		ResourceDimension		Dimension	   = ResourceDimension::NoDimension;
+		StorageResourceAccess	ResourceAccess = StorageResourceAccess::NoAccess;
+		uint32_t				DescriptorSet  = 0;
+		uint32_t				BindingPoint   = 0;
+		uint32_t				BindingCount   = 0;
+		uint32_t				RegisterSpace  = 0;
+	};
+
+	struct Attribute
+	{
+		std::string				Name		= {};
+		ReflectedShaderDataType Type		= {};
+		uint32_t				Binding		= {};
+		uint32_t				StreamIndex = {};
+	};
+
+	struct ReflectedBufferMember
+	{
+		std::string				Name	  = {};
+		size_t					Offset	  = 0;
+		size_t					Size	  = 0;
+		std::optional<uint32_t> ArraySize = {};
+		ShaderDataType			Type	  = {};
+	};
+
+	struct ReflectedUniformBuffer
+	{
+		std::string						   Name	   = {};
+		std::vector<ReflectedBufferMember> Members = {};
+	};
+
+	struct ReflectedStorageBuffer
+	{
+		std::string						   Name	   = {};
+		std::vector<ReflectedBufferMember> Members = {};
+	};
+
+	struct ShaderReflectionData
+	{
+		std::vector<Attribute>				Inputs;
+		std::vector<Attribute>				Outputs;
+		std::vector<ReflectedUniformBuffer> UniformBuffers = {};
+		std::vector<ReflectedStorageBuffer> StorageBuffers = {};
+		std::vector<ReflectedResource>		Resources	   = {};
+	};
+
+	struct ShaderAttribute
+	{
+		std::string	   Name;
+		ShaderDataType DataType;
+	};
+}	 // namespace Nexus::Graphics
