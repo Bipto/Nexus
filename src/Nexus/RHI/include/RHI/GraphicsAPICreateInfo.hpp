120c306ec424de62950a0011c3aef3b6d3a996e5
Adam Beardow <abeardow13@gmail.com>
2026-03-17 17:55:12 +0000

    Removed GraphicsAPI enum from GraphicsAPICreateInfo

--- src/Nexus/RHI/include/RHI/GraphicsAPICreateInfo.hpp
+++ src/Nexus/RHI/include/RHI/GraphicsAPICreateInfo.hpp
@@ -1,20 +1,9 @@
 #pragma once
 
+#include "RHI/APIVersion.hpp"
+
 namespace Nexus::Graphics
 {
-	/// @brief An enum class that represents an available graphics API backend
-	enum class GraphicsAPI
-	{
-		/// @brief Graphics will be created using OpenGL
-		OpenGL,
-
-		/// @brief Graphics will be created using Direct3D12
-		D3D12,
-
-		/// @brief Graphics will be created using Vulkan
-		Vulkan
-	};
-
 	struct GraphicsAPICreateInfo
 	{
 		GraphicsAPI API	  = GraphicsAPI::OpenGL;
