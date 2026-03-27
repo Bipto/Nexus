831d53f73efdc57fed72e53b3d7e6220ebe1f599
Adam Beardow <abeardow13@gmail.com>
2026-03-27 11:17:21 +0000

    Added function to check whether a shader lanauge is a binary format

--- src/Nexus/RHI/include/RHI/ShaderLanguage.hpp
+++ src/Nexus/RHI/include/RHI/ShaderLanguage.hpp
@@ -14,9 +14,20 @@ namespace Nexus::Graphics
 		/// @brief Value representing the HLSL shader language
 		HLSL,
 
-		/// @brief Value representing a SPIR-V binary shader
+		/// @brief Value representing a SPIR-V Vulkan compatible binary shader
 		Vulkan_SPIRV,
 
+		/// @brief Value representing a SPIR-V OpenGL compatible binary shader
 		OpenGL_SPIRV
 	};
+
+	inline bool IsBinaryShaderFormat(ShaderLanguage language)
+	{
+		switch (language)
+		{
+			case ShaderLanguage::Vulkan_SPIRV:
+			case ShaderLanguage::OpenGL_SPIRV: return true;
+			default: return false;
+		}
+	}
 }	 // namespace Nexus::Graphics
