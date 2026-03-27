080fe71490c7ab45770d364798137c55899a66bc
Adam Beardow <abeardow13@gmail.com>
2026-03-27 21:33:48 +0000

    Updated Python build script to use overlay port

--- scripts/build_emscripten.py
+++ scripts/build_emscripten.py
@@ -1,8 +1,11 @@
 import os
 
 def build():
-    os.system('emsdk_env && emcmake cmake -S ../ -B ../build/emscripten -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DNDEBUG=1 -DNX_PLATFORM_OPENGL=1 -DNX_PLATFORM_WEBGL=1 -DNX_PLATFORM_OPENAL=1 -DNX_BUILD_DEMO=1 && cmake --build ../build/emscripten --config Release')
+    overlay_path = os.path.abspath("../overlay-ports")
 
+    os.system('emsdk_env && emcmake cmake -S ../ -B ../build/emscripten -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_OVERLAY_PORTS=' + overlay_path + '  -DVCPKG_TARGET_TRIPLET=wasm32-emscripten -DCMAKE_BUILD_TYPE=Release -DNDEBUG=1 -DNX_PLATFORM_OPENGL=1 -DNX_PLATFORM_WEBGL=1 -DNX_PLATFORM_OPENAL=1 -DNX_BUILD_DEMO=1 && cmake --build ../build/emscripten --config Release')
+    #os.system('emsdk_env && emcmake cmake -S ../ -B ../build/emscripten -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=wasm32-emscripten -DCMAKE_BUILD_TYPE=Release -DNDEBUG=1 -DNX_PLATFORM_OPENGL=1 -DNX_PLATFORM_WEBGL=1 -DNX_PLATFORM_OPENAL=1 -DNX_BUILD_DEMO=1 && cmake --build ../build/emscripten --config Release')
+   
 if (__name__ == "__main__"):
     build()
     
