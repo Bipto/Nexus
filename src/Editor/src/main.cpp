294b3a748ff5a9e4a69413902b42535fd4e7fca0
Adam Beardow <abeardow13@gmail.com>
2026-04-20 21:30:10 +0100

    Integrated graphics rendering into Editor

--- src/Editor/src/main.cpp
+++ src/Editor/src/main.cpp
@@ -1,6 +1,11 @@
-// Start of wxWidgets "Hello World" Program
+#include <memory>
+
 #include <wx/aui/aui.h>
 #include <wx/wx.h>
+
+#include "RHI/GraphicsDevice.hpp"
+#include "RHI/IGraphicsAPI.hpp"
+#include "RHI/Swapchain.hpp"
 
 #ifdef _WIN32
 	#include <windows.h>
@@ -24,8 +29,26 @@ class MyFrame : public wxFrame
 	void OnHello(wxCommandEvent &event);
 	void OnExit(wxCommandEvent &event);
 	void OnAbout(wxCommandEvent &event);
+	void OnResize(wxSizeEvent &event);
+	void OnRenderTimer(wxTimerEvent &event);
+	void OnRender();
+	void OnPaint(wxPaintEvent &event);
+
+	void CreateGraphicsResources();
 
 	wxAuiManager m_mgr;
+	wxTimer		 m_RenderTimer = {};
+
+	std::unique_ptr<Nexus::Graphics::IGraphicsAPI>	  m_GraphicsAPI	   = {};
+	std::unique_ptr<Nexus::Graphics::IGraphicsDevice> m_GraphicsDevice = {};
+	Nexus::Graphics::CommandQueueHandle				  m_CommandQueue   = {};
+	Nexus::Graphics::SurfaceHandle					  m_Surface		   = {};
+	Nexus::Graphics::SwapchainHandle				  m_Swapchain	   = {};
+	Nexus::Graphics::CommandListHandle				  m_CommandList	   = {};
+
+	wxPanel *m_Panel		 = nullptr;
+	bool	 m_ResizePending = false;
+	wxSize	 m_PanelSize	 = {};
 };
 
 enum
@@ -46,17 +69,25 @@ bool MyApp::OnInit()
 	return true;
 }
 
-MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello World"), m_mgr(this)	   // <-- Initialize AUI manager
+MyFrame::MyFrame()
+	: wxFrame(nullptr, wxID_ANY, "Hello World", wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE | wxFULL_REPAINT_ON_RESIZE),
+	  m_mgr(this)	 // <-- Initialize AUI manager
 {
+	m_RenderTimer.SetOwner(this);
+	Connect(wxEVT_TIMER, wxTimerEventHandler(MyFrame::OnRenderTimer), NULL, this);
+
+	Bind(wxEVT_TIMER, &MyFrame::OnRenderTimer, this);
+	m_RenderTimer.Start(16);
+
 	// --- Central panel ---
-	wxPanel		 *panel = new wxPanel(this);
-	wxBoxSizer	 *sizer = new wxBoxSizer(wxVERTICAL);
-	wxStaticText *text1 = new wxStaticText(panel, wxID_ANY, "Hello, wxWidgets!");
-	sizer->Add(text1, 0, wxALL, 20);
-	panel->SetSizer(sizer);
+	m_Panel = new wxPanel(this);
+	m_Panel->Bind(wxEVT_SIZE, &MyFrame::OnResize, this);
+	m_Panel->SetDoubleBuffered(false);
+
+	m_Panel->SetBackgroundColour(wxBG_STYLE_CUSTOM);
 
 	// Add central panel to AUI
-	m_mgr.AddPane(panel, wxAuiPaneInfo().CenterPane());
+	m_mgr.AddPane(m_Panel, wxAuiPaneInfo().CenterPane());
 
 	// --- Dockable left panel ---
 	wxPanel *leftPanel = new wxPanel(this);
@@ -92,6 +123,8 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Hello World"), m_mgr(this)	   /
 	Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
 	Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
 	Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+	CreateGraphicsResources();
 }
 MyFrame::~MyFrame()
 {
@@ -111,4 +144,99 @@ void MyFrame::OnAbout(wxCommandEvent &event)
 void MyFrame::OnHello(wxCommandEvent &event)
 {
 	wxLogMessage("Hello world from wxWidgets!");
+}
+
+void MyFrame::OnResize(wxSizeEvent &event)
+{
+	if (!m_Swapchain.IsValid())
+	{
+		return;
+	}
+
+	wxSize size = event.GetSize();
+
+	if (size.GetWidth() == 0 || size.GetHeight() == 0)
+		return;
+
+	m_ResizePending = true;
+
+	m_PanelSize = size;
+
+	event.Skip();
+}
+
+void MyFrame::OnRenderTimer(wxTimerEvent &event)
+{
+	OnRender();
+	event.Skip();
+}
+
+void MyFrame::OnRender()
+{
+	if (m_ResizePending)
+	{
+		m_Swapchain->Resize(m_PanelSize.GetWidth(), m_PanelSize.GetHeight());
+		m_ResizePending = false;
+	}
+
+	m_CommandList->Begin();
+	m_CommandList->SetFramebuffer(m_Swapchain->GetCurrentFramebuffer());
+	m_CommandList->ClearColourTarget(0, {1.0f, 0.0f, 0.0f, 1.0f});
+	m_CommandList->End();
+
+	m_CommandQueue->SubmitCommandList(m_CommandList);
+	m_GraphicsDevice->WaitForIdle();
+
+	m_Swapchain->SwapBuffers({});
+}
+
+void MyFrame::OnPaint(wxPaintEvent &event)
+{
+	OnRender();
+	wxPaintDC dc(this);
+}
+
+void MyFrame::CreateGraphicsResources()
+{
+	Nexus::Graphics::GraphicsAPICreateInfo apiCreateInfo = {.API = Nexus::Graphics::GraphicsAPI::OpenGL, .Debug = true};
+	m_GraphicsAPI = std::unique_ptr<Nexus::Graphics::IGraphicsAPI>(Nexus::Graphics::IGraphicsAPI::CreateAPI(apiCreateInfo));
+
+	std::vector<std::shared_ptr<Nexus::Graphics::IPhysicalDevice>> physicalDevices = m_GraphicsAPI->GetPhysicalDevices();
+	m_GraphicsDevice = std::unique_ptr<Nexus::Graphics::IGraphicsDevice>(m_GraphicsAPI->CreateGraphicsDevice(physicalDevices[0]));
+
+	// iterate through all available command queues
+	std::vector<Nexus::Graphics::QueueFamilyInfo> queueFamilies = m_GraphicsDevice->GetQueueFamilies();
+	for (const Nexus::Graphics::QueueFamilyInfo &queueFamily : queueFamilies)
+	{
+		if (queueFamily.HasCapability(Nexus::Graphics::QueueCapabilities::Graphics) &&
+			queueFamily.HasCapability(Nexus::Graphics::QueueCapabilities::Compute) &&
+			queueFamily.HasCapability(Nexus::Graphics::QueueCapabilities::Transfer))
+		{
+			// create graphics queue
+			{
+				Nexus::Graphics::CommandQueueDescription queueDesc = {};
+				queueDesc.QueueFamilyIndex						   = queueFamily.QueueFamily;
+				queueDesc.QueueIndex							   = 0;
+				queueDesc.DebugName								   = "Application Graphics Queue";
+				m_CommandQueue									   = m_GraphicsDevice->CreateCommandQueue(queueDesc);
+			}
+		}
+	}
+
+	HWND hwnd = reinterpret_cast<HWND>(m_Panel->GetHandle());
+	HDC	 hdc  = ::GetDC(hwnd);
+
+	m_Surface = m_GraphicsDevice->CreateSurfaceFromWin32(reinterpret_cast<uintptr_t>(hwnd),
+														 reinterpret_cast<uintptr_t>(hdc),
+														 reinterpret_cast<uintptr_t>(GetModuleHandle(NULL)));
+
+	Nexus::Graphics::SwapchainDescription swapchainDesc = {};
+	swapchainDesc.Width									= m_Panel->GetSize().GetWidth();
+	swapchainDesc.Height								= m_Panel->GetSize().GetHeight();
+	swapchainDesc.Samples								= 1;
+	swapchainDesc.Surface								= m_Surface;
+
+	m_Swapchain = m_CommandQueue->CreateSwapchain(swapchainDesc);
+
+	m_CommandList = m_CommandQueue->CreateCommandList();
 }
