a227c33026d9b7a9965662e4f49a2467702d0f69
Adam Beardow <abeardow13@gmail.com>
2026-07-08 18:32:21 +0100

    Fixed compilation issues

--- src/Nexus/Logging/tests/src/LoggingTests.cpp
+++ src/Nexus/Logging/tests/src/LoggingTests.cpp
@@ -2,8 +2,19 @@
 
 #include "Logging/Logger.hpp"
 
+#include <memory>
+
+class TestLogSink : public Nexus::Logging::ILogSink
+{
+  public:
+	void LogMessage(const std::string &message) final
+	{
+	}
+};
+
 TEST(LoggingTest, TempTest)
 {
 	Nexus::Logging::Logger logger = {};
+	logger.RegisterSink(std::make_unique<TestLogSink>());
 	EXPECT_EQ(0, 0);
 }
